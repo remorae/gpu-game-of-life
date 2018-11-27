2fb2813a41d7fcbbeba6a172b683128f918e55b9
Alex Plagman <alexplagman@gmail.com>
2018-11-27 08:13:54 -0800

    Fix build

--- src/GPU-Game-of-Life/Utils.h
+++ src/GPU-Game-of-Life/Utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <cstddef>
 
 void randomizeGrid(std::vector<unsigned char>& grid, size_t width, size_t height);
 void clearGrid(std::vector<unsigned char>& grid);
